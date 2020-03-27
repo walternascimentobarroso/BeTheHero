a09b791a64e5bb0e0b1b99388a3edd755c423d46
Walter Nascimento Barroso <walternascimentobarroso@gmail.com>
2020-03-27 19:29:40 -0400

    [ADD] Add knex and create controllers

---
+++ backend/src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.list);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.list);
+
+routes.get('/incidents', IncidentController.list);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
